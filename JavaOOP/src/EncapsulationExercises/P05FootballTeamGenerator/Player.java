deea6725c0564061d3df261ed5b2c9def18ba613
mihailks <mihail_slavov@icloud.com>
2023-03-13 20:54:32 +0200

    OOP

--- JavaOOP/src/EncapsulationExercises/P05FootballTeamGenerator/Player.java
+++ JavaOOP/src/EncapsulationExercises/P05FootballTeamGenerator/Player.java
@@ -1,33 +1,46 @@
 package EncapsulationExercises.P05FootballTeamGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Player {
     private String name;
-    private List<Player> players;
+    private int endurance;
+    private int sprint;
+    private int dribble;
+    private int passing;
+    private int shooting;
 
-    public Player (String name){
-        setName(name);
-        players = new ArrayList<>();
+    public Player(String name, int endurance, int sprint, int dribble, int passing, int shooting) {
+        this.name = name;
+        this.endurance = endurance;
+        this.sprint = sprint;
+        this.dribble = dribble;
+        this.passing = passing;
+        this.shooting = shooting;
     }
 
-    private void setName(String name) {
-
+    private void setName(String name){
+        //TODO
     }
-
-    public String getName() {
+    public String getName(){
         return name;
     }
-    public void addPlayer(Player player){
-        players.add(player);
+
+    public void setEndurance(int endurance) {
+        this.endurance = endurance;
     }
-    public void removePlayer(String playerName){
-        boolean removed = players.removeIf(p -> p.getName().equals(playerName));
-        if (!removed){
-            String exceptionMessage = String.format("Player %s is not in %s team.",playerName,name);
-            throw new IllegalArgumentException(exceptionMessage);
-        }
+
+    public void setSprint(int sprint) {
+        this.sprint = sprint;
     }
-    //TODO...finish
+
+    public void setDribble(int dribble) {
+        this.dribble = dribble;
+    }
+
+    public void setPassing(int passing) {
+        this.passing = passing;
+    }
+
+    public void setShooting(int shooting) {
+        this.shooting = shooting;
+    }
 }
--- JavaOOP/src/EncapsulationExercises/P05FootballTeamGenerator/Team.java
+++ JavaOOP/src/EncapsulationExercises/P05FootballTeamGenerator/Team.java
@@ -1,4 +1,37 @@
 package EncapsulationExercises.P05FootballTeamGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private String name;
+    private List<Player> players;
+
+    public Team (String name){
+        setName(name);
+        players = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        //TODO
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void addPlayer(Player player){
+        players.add(player);
+    }
+    public void removePlayer(String playerName){
+        boolean removed = players.removeIf(p -> p.getName().equals(playerName));
+        if (!removed){
+            String exceptionMessage = String.format("Player %s is not in %s team.",playerName,name);
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+    }
+    public double getRating(){
+        //TODO
+
+        return 0;
+    }
 }
